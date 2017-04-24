a4543444174694ef6056773aa8cb1f295d4dba89
Matthew Funcke <matthew.funcke@derivco.co.za>
2017-04-24 14:43:58 +0200

    Added a FileInfo extension method for checking if a file is accessible - this should be used with care as File access can be volatile.

---
+++ CollectionOfHelpers/CollectionOfHelpers/FileExtensions/FileStatusChecks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionOfHelpers.FileExtensions
+{
+    public static class FileStatusChecks
+    {
+        /// <summary>
+        /// Returns true of the given file is:
+        ///  still being written to
+        ///  or being processed by another thread
+        ///  or does not exist (has already been processed)
+        /// IMPORTANT: File permissions (even file existence) are volatile — they can change at any time.
+        /// What this means is that you still have to be ready to handle the exception if file permissions
+        /// or existence are bad, in spite of using this check. 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static bool IsFileLocked(this FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
