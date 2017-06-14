89770bedfd922659065caa1cfae9458d2c0011ba
Matthew Funcke <matthew.funcke@derivco.co.za>
2017-06-14 15:42:13 +0200

    Added mass file renamer extension method that will perform a transform on an entire directory. As it is a static method with side-effects, an integration test needs to be added (a skeleton has been put in place, but it needs filling in).

---
+++ CollectionOfHelpers/CollectionOfHelpers/FileExtensions/FileModification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionOfHelpers.FileExtensions
+{
+    public static class FileModification
+    {
+        public delegate string NameTransformer(string filename);
+
+        /// <summary>
+        /// Rename each file in the given DirectoryInfo location (no children),
+        /// using the provided StringTransform.
+        /// Note that this is a static method with side effects, so use it with care.
+        /// </summary>
+        /// <param name="folderAddress"></param>
+        /// <param name="StringTransform"></param>
+        /// <param name="SkipIfFalse">If a file name doesn't cause this predicate to return true, it won't be renamed</param>
+        /// <returns>The total number of files in the directory that were transformed</returns>
+        public static int RenameFilesInFolder(this DirectoryInfo folderAddress, NameTransformer StringTransform,
+            Predicate<string> SkipIfFalse = null)
+        {
+            var total = 0;
+            foreach (var file in folderAddress.GetFiles("*"))
+            {
+                if (SkipIfFalse != null && !SkipIfFalse(file.Name))
+                {
+                    continue;
+                }
+                total++;
+
+                Directory.Move(file.FullName, file.Directory.FullName + "\\" + StringTransform(file.Name));
+            }
+            return total;
+        }
+    }
+}
---
+++ CollectionOfHelpers/CollectionOfHelpersTests/FileExtensions/FileModificationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CollectionOfHelpersTests.FileExtensions
+{
+    [TestFixture]
+    public class FileModificationTests
+    {
+        //Feedback about testing a static method with side effects:
+        //Avoid it when possible. If you can't then these are some options:
+        //So typically, I create an interface and a wrapper class for the static class
+        //and I always interact with it via the interface
+        //so then in tests I can simple subst the interface
+        //and then I can have one or two simple tests to validate the wrapper if possible
+        //
+        //alternately just put some files in a TestData folder in your project and write 
+        //a test the evals that the files have been moved
+
+        [Test]
+        public void RenameFilesInFolder_NoChangesTest()
+        {
+            //arrange
+            try
+            {
+                //TODO: create several on-the-fly files that will be cleared away afterwards
+
+                //act
+                //TODO: try and move the files
+
+                //assert
+                //TODO: check that the files move as expected
+            }
+            finally
+            {
+                //cleanup
+                //TODO: clear away all files in the dynamic file-directory
+            }
+        }
+    }
+}
