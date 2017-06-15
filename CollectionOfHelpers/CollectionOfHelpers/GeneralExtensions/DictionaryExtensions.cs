0aa6eb5053cb90a7ba368967a8073aeeee7682fb
MICHELLE-TOWER\Michelle <kogelmichelle@gmail.com>
2017-06-15 20:12:55 +0200

    Added tryadd extension to dictionary class

---
+++ CollectionOfHelpers/CollectionOfHelpers/GeneralExtensions/DictionaryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionOfHelpers.GeneralExtensions
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Add the key-value pair to the dictionary and return true if the key doesn't already exist in the dictionary. Return false if the key already exists.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="Dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> Dictionary, TKey key, TValue value)
+        {
+            if (!Dictionary.ContainsKey(key))
+            {
+                Dictionary.Add(key, value);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
