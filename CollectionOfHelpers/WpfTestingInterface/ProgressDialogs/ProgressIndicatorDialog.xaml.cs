77d06d3c3987453dd3c0877f52be079cf4b5b6ab
MICHELLE-TOWER\Michelle <kogelmichelle@gmail.com>
2017-06-20 19:34:10 +0200

    Added windows that will be used to create the different show progress types. No code as yet.

---
+++ CollectionOfHelpers/WpfTestingInterface/ProgressDialogs/ProgressIndicatorDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTestingInterface.ProgressDialogs
+{
+    /// <summary>
+    /// Interaction logic for ProgressIndicatorDialog.xaml
+    /// </summary>
+    public partial class ProgressIndicatorDialog : Window
+    {
+        public ProgressIndicatorDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
